0a0d1ede0cf60d549576045015d6ff8ab62139ed
AndreSecco <wellington@i9ss.com.br>
2022-08-11 08:49:47 -0300

    Alteração Final (Excluir e Editar lista)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,89 @@
-class Produto{
-    constructor(){
+class Produto {
+    constructor() {
         this.id = 1
         this.arrayProdutos = []
+        this.editId = null
     }
-    salvar(){
+    salvar() {
         let produto = this.lerDados()
         //this.validaCampos(produto)
 
-        if(this.validaCampos(produto)){
-            this.adicionar(produto)
+        if (this.validaCampos(produto)) {
+            if(this.editId == null){
+                this.adicionar(produto)
+            }else{
+                this.atualizar(this.editId, produto)
+            }
         }
 
         this.listaTabela()
+        this.cancelar()
     }
 
-    listaTabela(){
+    listaTabela() {
         let tbody = document.getElementById('tbody')
 
-        for(let i = 0; i < this.arrayProdutos.length; i++){
+        tbody.innerText = ''
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
             let tr = tbody.insertRow()
             let td_id = tr.insertCell()
             let td_produto = tr.insertCell()
             let td_preco = tr.insertCell()
-            let td_acao = tr.insertCell()
+            let td_acoes = tr.insertCell()
+
+            td_id.innerText = this.arrayProdutos[i].id
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto
+            td_preco.innerText = this.arrayProdutos[i].preco
+
+            td_id.classList.add('center')
+
+            let imgEdit = document.createElement('img')
+            imgEdit.src = 'img/btn-editar.png';
+            imgEdit.style.cursor = 'pointer'
+
+            let imgDelete = document.createElement('img')
+            imgDelete.src = 'img/btn-deletar.png'
+            imgDelete.style.cursor = 'pointer'
+
+            imgDelete.setAttribute('onclick', "produto.deletar(" + this.arrayProdutos[i].id + ")")
+
+            imgEdit.setAttribute("onclick", "produto.preparaEdicao(" + JSON.stringify(this.arrayProdutos[i]) + ")")
+
+            td_acoes.appendChild(imgEdit)
+
+            td_acoes.appendChild(imgDelete)
 
         }
     }
 
-    adicionar(produto){
+    adicionar(produto) {
         this.arrayProdutos.push(produto)
         this.id++
     }
 
-    lerDados(){
+    atualizar(id, produto){
+        for(let i = 0; i < this.arrayProdutos.length; i++){
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto
+                this.arrayProdutos[i].preco = produto.preco
+            }
+        }
+        // this.listaTabela()
+    }
+
+    preparaEdicao(dados){
+        //alert(dados.id)
+        this.editId = dados.id
+        document.getElementById('produto').value = dados.nomeProduto
+        document.getElementById('preco').value = dados.preco
+
+        document.getElementById('btn-salvar').innerText = 'Atualizar'
+    }
+
+    lerDados() {
         let produto = {}
-        
+
         produto.id = this.id
         produto.nomeProduto = document.getElementById('produto').value
         produto.preco = document.getElementById('preco').value
@@ -42,16 +91,16 @@ class Produto{
         return produto
     }
 
-    validaCampos(produto){
+    validaCampos(produto) {
         let msg = ''
-        if(produto.nomeProduto == ''){
+        if (produto.nomeProduto == '') {
             msg += '- Informe o nome do Produto '
         }
-        if(produto.preco == ''){
+        if (produto.preco == '') {
             msg += '- Informe o preço do Produto '
         }
 
-        if(msg != ''){
+        if (msg != '') {
             alert(msg)
             return false
         }
@@ -59,8 +108,26 @@ class Produto{
         return true
 
     }
-    cancelar(){
-        
+    cancelar() {
+        document.getElementById('produto').value = ''
+        document.getElementById('preco').value = ''
+
+        if(this.editId != null){
+            document.getElementById('btn-salvar').innerText = 'Salvar'
+            this.editId = null
+        }
+    }
+
+    deletar(id) {
+        if (confirm('Deseja deletar o produto do ID ' + id)) {
+            for (let i = 0; i < this.arrayProdutos.length; i++) {
+                if (this.arrayProdutos[i].id == id) {
+                    this.arrayProdutos.splice(i, 1)
+                    tbody.deleteRow(i)
+                }
+            }
+        }
+
     }
 }
 let produto = new Produto()
